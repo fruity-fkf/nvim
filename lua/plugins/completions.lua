bd4cbf8750166c19a335ca15a2e2481a19dbdcb1
fruity-fkf <fkf.6106@gmail.com>
2024-01-03 12:14:32 +0000

    added path completion

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,51 +1,65 @@
-
-
 return {
-  {
-    "L3MON4D3/LuaSnip",
-    lazy = false,
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      "saadparwaiz1/cmp_luasnip",
-    },
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
-  },
-  {
-    "hrsh7th/cmp-nvim-lsp",
-    lazy = false,
-    config = true,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    lazy = false,
-    config = function()
-      local cmp = require("cmp")
-      cmp.setup({
-        window = {
-          documentation = cmp.config.window.bordered(),
-          completion = cmp.config.window.bordered(),
-        },
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
-    end,
-  },
+	{
+		"hrsh7th/cmp-path",
+		config = function()
+			require("cmp").setup({
+				sources = {
+					{ name = "path" },
+					option = {
+						-- Options go into this table
+
+						trailing_slash = true,
+					},
+				},
+			})
+		end,
+	},
+
+	{
+		"L3MON4D3/LuaSnip",
+		lazy = false,
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			require("luasnip.loaders.from_vscode").lazy_load()
+		end,
+	},
+	{
+		"hrsh7th/cmp-nvim-lsp",
+		lazy = false,
+		config = true,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		lazy = false,
+		config = function()
+			local cmp = require("cmp")
+			cmp.setup({
+				window = {
+					documentation = cmp.config.window.bordered(),
+					completion = cmp.config.window.bordered(),
+				},
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }

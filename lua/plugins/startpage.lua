673b010e762c4e1c95bf5d5304636def7c8a5c58
fruity-fkf <fkf.6106@gmail.com>
2024-01-04 14:42:38 +0000

    bleh :p

--- lua/plugins/startpage.lua
+++ lua/plugins/startpage.lua
@@ -1,7 +1,7 @@
 return {
-  "startup-nvim/startup.nvim",
-  requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
-  config = function()
-    require("startup").setup({theme = "dashboard"}) -- theme name here from https://github.com/startup-nvim/startup.nvim
-  end
+	"startup-nvim/startup.nvim",
+	requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	config = function()
+		require("startup").setup({ theme = "dashboard" }) -- theme name here from https://github.com/startup-nvim/startup.nvim
+	end,
 }

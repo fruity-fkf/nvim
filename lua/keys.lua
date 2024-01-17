37591fb341290417c398bbc1ec7f5092f58401b2
fkf <fkf.6106@gmail.com>
2024-01-17 23:31:51 +0600

    keybind tweaks :3

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
---vim.api.nvim_set_keymap("n", "<leader>", "", {})
-vim.api.nvim_set_keymap("n", "<leader>rn", ":RnvimrToggle<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>mm", ":MinimapToggle<CR>", {})
+--vim.keymap.set("n", "<leader>", "", {})
+vim.keymap.set("n", "<leader>rn", ":RnvimrToggle<CR>", {})
+vim.keymap.set("n", "<leader>mm", ":MinimapToggle<CR>", {})
 
 vim.keymap.set("n", "<leader>S", '<cmd>lua require("spectre").toggle()<CR>', {
 	desc = "Toggle Spectre",
@@ -16,19 +16,24 @@ vim.keymap.set("n", "<leader>sp", '<cmd>lua require("spectre").open_file_search(
 	desc = "Search on current file",
 })
 
-vim.api.nvim_set_keymap("n", "<leader>Tf", ":ToggleTerm direction =float<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>Tv", ":ToggleTerm direction =vertical<CR>", {})
-vim.api.nvim_set_keymap("n", "<leader>Th", ":ToggleTerm direction =horizontal<CR>", {})
+-- terminal
 
-vim.api.nvim_set_keymap("n", "<leader>tc", ":tabclose<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>to", ":tabonly<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>tn", ":tabnew<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>tp", ":tabp<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>Tf", ":ToggleTerm direction =float<CR>", {})
+vim.keymap.set("n", "<leader>Tv", ":ToggleTerm direction =vertical<CR>", {})
+vim.keymap.set("n", "<leader>Th", ":ToggleTerm direction =horizontal<CR>", {})
+
+vim.keymap.set("n", "<leader>tc", ":tabclose<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>to", ":tabonly<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>tn", ":tabnew<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>tp", ":tabp<CR>", { noremap = true })
 -- move current tab to previous position
-vim.api.nvim_set_keymap("n", "<leader>tmp", ":-tabmove<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>tmp", ":-tabmove<CR>", { noremap = true })
 -- move current tab to next position
-vim.api.nvim_set_keymap("n", "<leader>tmn", ":+tabmove<CR>", { noremap = true })
+vim.keymap.set("n", "<leader>tmn", ":+tabmove<CR>", { noremap = true })
+
 vim.keymap.set("n", "<leader>ft", ":Neotree toggle<CR>", {})
+vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 vim.keymap.set("n", "<leader>ff", ":Telescope live_grep<CR>", {})

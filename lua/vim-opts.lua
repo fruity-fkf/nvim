f5d6c795517a1ae5953a827be9bd06d1a722ec1f
fruity-fkf <fkf.6106@gmail.com>
2024-04-06 16:41:25 +0600

    I forgor what I added ;-;

--- lua/vim-opts.lua
+++ lua/vim-opts.lua
@@ -1,6 +1,6 @@
 vim.opt.sessionoptions = "curdir,folds,globals,help,tabpages,terminal,winsize"
 vim.opt.conceallevel = 1
-vim.api.nvim_set_option("clipboard", "unnamed")
+vim.opt.termguicolors = true
 vim.cmd("set mouse=a")
 vim.cmd("set undodir=~/.local/tmp")
 vim.cmd("set undofile")
@@ -9,6 +9,10 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.opt.fillchars = { eob = " " }
+
+vim.opt.clipboard = "unnamedplus"
+vim.wo.relativenumber = true
 vim.g.mapleader = " "
 
 -- Navigate vim panes better

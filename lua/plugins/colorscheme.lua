d44d6edb42707c6abe0bc4d26a8ba3c5da00970e
fruity-fkf <fkf.6106@gmail.com>
2024-05-05 12:27:55 +0600

    hard like my dick

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,43 +1,52 @@
 return {
-	{ "LunarVim/Colorschemes" },
-	{ "sainnhe/edge" },
-	{ "navarasu/onedark.nvim" },
-	{ "shaunsingh/nord.nvim" },
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",
-	},
+  { "LunarVim/Colorschemes" },
+  { "sainnhe/edge" },
+  { "navarasu/onedark.nvim" },
+  { "shaunsingh/nord.nvim" },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+  },
 
-	{
-		"rebelot/kanagawa.nvim",
-	},
+  {
+    "rebelot/kanagawa.nvim",
+  },
 
-	{
-		"sainnhe/gruvbox-material",
+  {
+    "sainnhe/gruvbox-material",
 
-		config = function()
-			local gruvbox_material_background = "soft"
-		end,
-	},
+    config = function()
+      local gruvbox_material_background = "soft"
+    end,
+  },
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        contrast = "hard",
+        transparent_mode = false,
+      })
+      vim.cmd.colorscheme('gruvbox')
+    end
+  },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  {
+    "LunarVim/Colorschemes",
+  },
 
-	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = function() end },
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-	{
-		"LunarVim/Colorschemes",
-	},
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function() end,
+  },
 
-	{
-		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
-		opts = {},
-		config = function() end,
-	},
-
-	{
-		"andrew-george/telescope-themes",
-		config = function()
-			require("telescope").load_extension("themes")
-		end,
-	},
+  {
+    "andrew-george/telescope-themes",
+    config = function()
+      require("telescope").load_extension("themes")
+    end,
+  },
 }

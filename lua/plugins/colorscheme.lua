212a92c0c17a8143fccb2b894ce9eea3fcba1d7e
FKF <fkf.6106@gmail.com>
2024-08-01 18:38:11 +0600

    idk what I did

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,66 +1,78 @@
 return {
-  { "LunarVim/Colorschemes" },
+	{ "LunarVim/Colorschemes" },
 
-  {
-    "scottmckendry/cyberdream.nvim",
-    lazy = false,
-    priority = 1000,
-  },
-  { "sainnhe/edge" },
-  { "navarasu/onedark.nvim" },
-  { "shaunsingh/nord.nvim" },
-  {
-    "rose-pine/neovim",
-    name = "rose-pine",
-  },
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
+	},
 
-  {
-    "rebelot/kanagawa.nvim",
-  },
+	{ "sainnhe/edge" },
+	{ "navarasu/onedark.nvim" },
+	{ "shaunsingh/nord.nvim" },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+	},
 
+	{
+		"rebelot/kanagawa.nvim",
+	},
 
-  {
-    'sainnhe/gruvbox-material',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- Optionally configure and load the colorscheme
-      -- directly inside the plugin declaration.
-      vim.g.gruvbox_material_background = "hard"
-    end
-  },
+	{
+		"sainnhe/gruvbox-material",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			-- Optionally configure and load the colorscheme
+			-- directly inside the plugin declaration.
+			vim.g.gruvbox_material_background = "hard"
+		end,
+	},
 
+	{
+		"ellisonleao/gruvbox.nvim",
+		priority = 1000,
+		opts = {
+			transparent = true,
+			styles = {
+				sidebars = "transparent",
+				floats = "transparent",
+			},
+		},
 
+		config = function()
+			require("gruvbox").setup({
+				contrast = "hard",
+				transparent_mode = false,
+			})
+			vim.cmd.colorscheme("gruvbox")
+		end,
+	},
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+		"LunarVim/Colorschemes",
+	},
 
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {},
+		config = function() end,
+	},
 
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        contrast = "hard",
-        transparent_mode = false,
-      })
-      vim.cmd.colorscheme('gruvbox')
-    end
-  },
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  {
-    "LunarVim/Colorschemes",
-  },
-
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function() end,
-  },
-
-  {
-    "andrew-george/telescope-themes",
-    config = function()
-      require("telescope").load_extension("themes")
-    end,
-  },
+	{
+		"andrew-george/telescope-themes",
+		config = function()
+			require("telescope").load_extension("themes")
+		end,
+	},
 }

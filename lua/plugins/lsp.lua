baa7a838ec154374df15e7ab4e1488819cd3a55d
fruity-fkf <fkf.6106@gmail.com>
2024-10-08 10:16:01 +0200

    Made it better for nix

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,32 +1,30 @@
 return {
 
-  {
-    "williamboman/mason.nvim",
+	{
+		"williamboman/mason.nvim",
 
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
 
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd" },
-      })
-    end,
-  },
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({})
+		end,
+	},
 
-  {
+	{
 
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.pyright.setup({})
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.pyright.setup({})
 
-      lspconfig.clangd.setup({})
+			lspconfig.clangd.setup({})
 
-      lspconfig.lua_ls.setup({})
-    end,
-  },
+			lspconfig.lua_ls.setup({})
+		end,
+	},
 }

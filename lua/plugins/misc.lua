21bc10d626eb0453124fd59e1a1a44c5aeca310e
FKF <fkf.6106@gmail.com>
2024-10-20 16:33:35 +0000

    skibidi

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,57 +1,61 @@
 return {
+	{ "nvchad/volt", lazy = true },
+	{
+		"nvchad/minty",
+		cmd = { "Shades", "Huefy" },
+	},
 
-  {
-    "norcalli/nvim-colorizer.lua",
-    config = function()
-      require 'colorizer'.setup()
-    end
-  },
-  {
-    "folke/todo-comments.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    },
-  },
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 
-  { "akinsho/toggleterm.nvim", version = "*", opts = {} },
-  -- {
-  -- 	"wfxr/minimap.vim",
-  -- 	build = "cargo install --locked code-minimap",
-  -- 	cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
-  -- 	config = function()
-  -- 		vim.cmd("let g:minimap_width = 10")
-  -- 		vim.cmd("let g:minimap_auto_start = 1")
-  -- 		vim.cmd("let g:minimap_auto_start_win_enter = 1")
-  -- 	end,
-  -- },
+	{ "akinsho/toggleterm.nvim", version = "*", opts = {} },
+	-- {
+	-- 	"wfxr/minimap.vim",
+	-- 	build = "cargo install --locked code-minimap",
+	-- 	cmd = { "Minimap", "MinimapClose", "MinimapToggle", "MinimapRefresh", "MinimapUpdateHighlight" },
+	-- 	config = function()
+	-- 		vim.cmd("let g:minimap_width = 10")
+	-- 		vim.cmd("let g:minimap_auto_start = 1")
+	-- 		vim.cmd("let g:minimap_auto_start_win_enter = 1")
+	-- 	end,
+	-- },
 
-  {
-    "christoomey/vim-tmux-navigator",
-    cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-    },
-    keys = {
-      { "<c-h>",  "<cmd><C-U>TmuxNavigateLeft<cr>" },
-      { "<c-j>",  "<cmd><C-U>TmuxNavigateDown<cr>" },
-      { "<c-k>",  "<cmd><C-U>TmuxNavigateUp<cr>" },
-      { "<c-l>",  "<cmd><C-U>TmuxNavigateRight<cr>" },
-      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-    },
-  },
+	{
+		"christoomey/vim-tmux-navigator",
+		cmd = {
+			"TmuxNavigateLeft",
+			"TmuxNavigateDown",
+			"TmuxNavigateUp",
+			"TmuxNavigateRight",
+			"TmuxNavigatePrevious",
+		},
+		keys = {
+			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+		},
+	},
 
-  {
-    "windwp/nvim-spectre",
-    event = "BufRead",
-    config = function()
-      require("spectre").setup()
-    end,
-  },
-
+	{
+		"windwp/nvim-spectre",
+		event = "BufRead",
+		config = function()
+			require("spectre").setup()
+		end,
+	},
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,6 +1,17 @@
 return {
 
 	{
+		"nvchad/showkeys",
+		cmd = "ShowkeysToggle",
+		opts = {
+			timeout = 1,
+			maxkeys = 5,
+			position = "top-center",
+			-- more opts
+		},
+	},
+
+	{
 		"rcarriga/nvim-notify",
 		keys = {
 			{

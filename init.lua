37b729d331538f6cc1c63278c41f6240c3bec6c4
fruity-fkf <fruity.fkf@proton.me>
2024-10-23 20:24:09 +0200

    nix stuff

--- init.lua
+++ init.lua
@@ -3,15 +3,36 @@ require("tele")
 require("keys")
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup("plugins")
 require("current-theme")
+require("lspconfig").nixd.setup({
+	cmd = { "nixd" },
+	settings = {
+		nixd = {
+			nixpkgs = {
+				expr = "import <nixpkgs> { }",
+			},
+			formatting = {
+				command = { "nixfmt" }, -- or nixfmt or nixpkgs-fmt
+			},
+			-- options = {
+			--   nixos = {
+			--       expr = '(builtins.getFlake "/PATH/TO/FLAKE").nixosConfigurations.CONFIGNAME.options',
+			--   },
+			--   home_manager = {
+			--       expr = '(builtins.getFlake "/PATH/TO/FLAKE").homeConfigurations.CONFIGNAME.options',
+			--   },
+			-- },
+		},
+	},
+})

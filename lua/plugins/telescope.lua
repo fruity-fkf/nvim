c1317af0d07c915f199c3b45b68745a73a056904
fruity-fkf <fruity.fkf@proton.me>
2024-10-23 20:30:17 +0200

    fzf

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,53 +1,100 @@
 return {
+
+	-- NOTE: FZF FOR....REASONS
+
+	{ "junegunn/fzf.vim" },
+
+	{ "junegunn/fzf" },
+
 	{
 		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		-- or                              , branch = '0.1.x',
-		dependencies = { "nvim-lua/plenary.nvim", "debugloop/telescope-undo.nvim" },
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+		},
+
 		config = function()
-			local builtin = require("telescope.builtin")
-			require("telescope").setup({
-				-- the rest of your telescope config goes here
-				extensions = {
-					undo = {
-						-- telescope-undo.nvim config, see below
+			local telescope = require("telescope")
+			local actions = require("telescope.actions")
+
+			telescope.setup({
+				defaults = {
+					layout_strategy = "horizontal",
+					layout_config = {
+						horizontal = {
+							width = 0.9,
+							height = 0.9,
+							prompt_position = "top",
+							preview_cutoff = 120,
+							preview_width = 75,
+							horizontal = { mirror = false },
+							vertical = { mirror = false },
+						},
 					},
-					-- other extensions:
-					-- file_browser = { ... }
+					find_command = {
+						"rg",
+						"--no-heading",
+						"--with-filename",
+						"--line-number",
+						"--column",
+						"--smart-case",
+					},
+					prompt_prefix = "  ",
+					selection_caret = "  ",
+					entry_prefix = "  ",
+					initial_mode = "insert",
+					selection_strategy = "reset",
+					sorting_strategy = "ascending",
+					file_sorter = require("telescope.sorters").get_fuzzy_file,
+					file_ignore_patterns = {},
+					generic_sorter = require("telescope.sorters").get_generic_fuzzy_sorter,
+					path_display = {},
+					winblend = 0,
+					border = {},
+					borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+					color_devicons = true,
+					use_less = true,
+					set_env = { ["COLORTERM"] = "truecolor" }, -- default = nil,
+					file_previewer = require("telescope.previewers").vim_buffer_cat.new,
+					grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
+					qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+					buffer_previewer_maker = require("telescope.previewers").buffer_previewer_maker,
+					mappings = {
+						i = {
+							["<C-n>"] = actions.move_selection_next,
+							["<C-p>"] = actions.move_selection_previous,
+							["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+							["<esc>"] = actions.close,
+							["<CR>"] = actions.select_default + actions.center,
+						},
+						n = {
+							["<C-n>"] = actions.move_selection_next,
+							["<C-p>"] = actions.move_selection_previous,
+							["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
+						},
+					},
 				},
 			})
-			require("telescope").load_extension("undo")
+
+			telescope.load_extension("fzf")
 		end,
 	},
 
+	-- telescope ui select extensions
 	{
 		"nvim-telescope/telescope-ui-select.nvim",
-
 		config = function()
-			require("telescope").setup({
+			local telescope = require("telescope")
+			telescope.setup({
 				extensions = {
 					["ui-select"] = {
 						require("telescope.themes").get_dropdown({
 							-- even more opts
 						}),
-
-						-- pseudo code / specification for writing custom displays, like the one
-						-- for "codeactions"
-						-- specific_opts = {
-						--   [kind] = {
-						--     make_indexed = function(items) -> indexed_items, width,
-						--     make_displayer = function(widths) -> displayer
-						--     make_display = function(displayer) -> function(e)
-						--     make_ordinal = function(e) -> string
-						--   },
-						--   -- for example to disable the custom builtin "codeactions" display
-						--      do the following
-						--   codeactions = false,
-						-- }
 					},
 				},
 			})
-
 			require("telescope").load_extension("ui-select")
 		end,
 	},

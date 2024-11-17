e1656aad14be271eeb73c442ce172e2007e7a627
fruity-fkf <fruity.fkf@proton.me>
2024-11-17 12:10:03 +0100

    Vro should work now

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -85,18 +85,12 @@ return {
 		end,
 	},
 
+	-- lazy.nvim
 	{
 		"folke/noice.nvim",
 		event = "VeryLazy",
 		opts = {
 			-- add any options here
-		},
-		dependencies = {
-			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-			"MunifTanjim/nui.nvim",
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
 		},
 		require("noice").setup({
 			views = {
@@ -112,6 +106,14 @@ return {
 				},
 			},
 		}),
+
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+		},
 	},
 
 	-- {

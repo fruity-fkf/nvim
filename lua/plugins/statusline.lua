06821e97078f72f4e3da611a66cedadf0a5bd983
fruity-fkf <fruity.fkf@proton.me>
2024-11-27 02:19:51 +0100

    did shit

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,4 +1,5 @@
 return {
+
 	"nvim-lualine/lualine.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
@@ -6,19 +7,8 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = "auto",
-				component_separators = { left = "", right = "" },
-				section_separators = { left = "", right = "" },
-				disabled_filetypes = {
-					statusline = {},
-					winbar = {},
-				},
-				ignore_focus = {},
-				always_divide_middle = true,
-				globalstatus = false,
-				refresh = {
-					statusline = 1000,
-					winbar = 1000,
-				},
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
 			},
 			sections = {
 				lualine_a = { "mode" },
@@ -28,15 +18,49 @@ return {
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = { "filename" },
-				lualine_x = { "location" },
-				lualine_y = {},
-				lualine_z = {},
-			},
-			extensions = {},
 		})
 	end,
 }
+
+-- return {
+-- 	"nvim-lualine/lualine.nvim",
+-- 	dependencies = { "nvim-tree/nvim-web-devicons" },
+-- 	config = function()
+-- 		require("lualine").setup({
+-- 			options = {
+-- 				icons_enabled = true,
+-- 				theme = "auto",
+-- 				component_separators = { left = "", right = "" },
+-- 				section_separators = { left = "", right = "" },
+-- 				disabled_filetypes = {
+-- 					statusline = {},
+-- 					winbar = {},
+-- 				},
+-- 				ignore_focus = {},
+-- 				always_divide_middle = true,
+-- 				globalstatus = false,
+-- 				refresh = {
+-- 					statusline = 1000,
+-- 					winbar = 1000,
+-- 				},
+-- 			},
+-- 			sections = {
+-- 				lualine_a = { "mode" },
+-- 				lualine_b = { "branch", "diff", "diagnostics" },
+-- 				lualine_c = { "filename" },
+-- 				lualine_x = { "encoding", "fileformat", "filetype" },
+-- 				lualine_y = { "progress" },
+-- 				lualine_z = { "location" },
+-- 			},
+-- 			inactive_sections = {
+-- 				lualine_a = {},
+-- 				lualine_b = {},
+-- 				lualine_c = { "filename" },
+-- 				lualine_x = { "location" },
+-- 				lualine_y = {},
+-- 				lualine_z = {},
+-- 			},
+-- 			extensions = {},
+-- 		})
+-- 	end,
+-- }

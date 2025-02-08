bc762ae5aaa0b4beb5f205501505df6c277853d9
fruity-fkf <fruity.fkf@proton.me>
2025-02-08 07:48:46 +0100

    Changes changes changes......

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,20 +6,6 @@ return {
 
 		keys = {
 			{
-				"<leader>z",
-				function()
-					Snacks.zen()
-				end,
-				desc = "Toggle Zen Mode",
-			},
-			{
-				"<leader>Z",
-				function()
-					Snacks.zen.zoom()
-				end,
-				desc = "Toggle Zoom",
-			},
-			{
 				"<leader>.",
 				function()
 					Snacks.scratch()

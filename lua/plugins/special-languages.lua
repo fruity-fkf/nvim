27e4e29487548cfb1100a0e08b2a76b8d727017d
fkf <fkf.6106@gmail.com>
2025-04-21 10:17:37 -0700

    the plugin

--- lua/plugins/special-languages.lua
+++ lua/plugins/special-languages.lua
@@ -1,4 +1,17 @@
 return {
+
+
+  {
+    "fruity-fkf/assembly-cheatsheet.vim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  config = function()
+    vim.api.nvim_create_user_command("AssemblyCheatsheet", function()
+      require("assembly_cheatsheet").cheatsheet()
+    end, {})
+  end
+},
+
+
   {
     "mrcjkb/rustaceanvim",
     version = "^6", -- Recommended
